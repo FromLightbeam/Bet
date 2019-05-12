e841f420bfff9fb8c3fb8709638f6efdb51828ca
Andrew <fromlightbeam@gmail.com>
2019-05-13 01:16:11 +0300

    add padding to details matches

--- front/src/components/match/MatchDetails/index.js
+++ front/src/components/match/MatchDetails/index.js
@@ -11,33 +11,34 @@ import "./style.scss";
 
 function MatchDetails(props) {
   const { match, metrics } = props;
-
+  console.log(metrics)
   return (
-    <Paper className='item match-details__header'>
+    <Paper className='item match-details__paper'>
       {match.club_1 ?
         <h2>{`${match.club_1.name} - ${match.club_2.name}`}</h2> : null
       }
-
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Metrics</TableCell>
-            <TableCell>Value</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {metrics.map(row => (
-            <TableRow key={row.id}>
-              <TableCell>
-                {row.metric.shortname}
-              </TableCell>
-              <TableCell>
-                {row.value}
-              </TableCell>
+      <div className='match-details__table'>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Metrics</TableCell>
+              <TableCell>Value</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {metrics.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>
+                  {row.metric.shortname} - {row.metric.description}
+                </TableCell>
+                <TableCell>
+                  {row.value}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </Paper>
   );
 }
